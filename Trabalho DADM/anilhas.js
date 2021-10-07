eeba1231e0e99ef6fe4cfaac3b29ef184bc428e0
Gabriela Xavier <gabrielaxavierfontes@unigranrio.br>
2021-10-07 16:12:55 -0300

    js feito

--- Trabalho DADM/anilhas.js
+++ Trabalho DADM/anilhas.js
@@ -2,6 +2,7 @@ var produtos = [];
 var itens = [];
 var item = 0;
 var padrao = 90;
+var soma = 0;  
 
 function atrib(){
     const anilha = document.querySelector('input[name=anilha]:checked').value;
@@ -9,21 +10,15 @@ function atrib(){
     const presilha = document.querySelector('input[name=presilha]:checked').value;
     const luva = document.querySelector('input[name=luva]:checked').value;
     const almofada = document.querySelector('input[name=almofada]:checked').value;
-    produtos = [anilha, barra, almofada, presilha, luva]
-    iter(produtos)
-};
-
-function soma(itens){
-    for(var i = 0; i < itens.length; i++) {
-        soma += itens[i];
-    }
-    document.getElementById('resultado') = `R$ ${padrao + soma}`;
+    produtos = [anilha, barra, almofada, presilha, luva];
+    iter(produtos);
+    reset(produtos);
 };
 
 function iter(prod){
     for (var i = 0; i < prod.length; i++) {
         var item = 0;
-        var label = prod[i]
+        var label = prod[i];
         var b = document.querySelector(`label[for=${label}]`).textContent;
         b = b.split(' ');
         b = b[b.length - 1];
@@ -40,6 +35,16 @@ function iter(prod){
         itens.push(item);
     }
     console.log(itens);
-    soma(itens);
+    for(var i = 0; i < itens.length; i++) {
+        soma += itens[i];
+    };
+    var total = String(padrao + soma)
+    document.getElementById('resultado').value = `O valor total da sua compra é: R$${total},00`
 };
 
+function reset(itens){
+    for(var i = 0; i < itens.length; i++) {
+        var radio = document.querySelector(`input[type=radio][value=${itens[i]}]:checked`);
+        radio.checked = false;
+    };
+}
